873bbb870db61fd4405a0ec85252e38d3b246719
Gabriela Ossowska <gab.ossowska@gmail.com>
2022-01-27 00:09:52 +0100

    fixed moving script

--- LL4/experiments/move.sh
+++ LL4/experiments/move.sh
@@ -5,7 +5,7 @@ for exp_type in */ ; do
 	mkdir ../results
 	for experiment in */ ; do
 		cd $experiment
-		cp worker_0.simple_rl_graph.main_level.main_level.agent_0.csv "../../results/${experiment::-1}"
+		cp worker_0.simple_rl_graph.main_level.main_level.agent_0.csv "../../results/${experiment::-1}.csv"
 		cd ..
 	done
 	cd ../..
