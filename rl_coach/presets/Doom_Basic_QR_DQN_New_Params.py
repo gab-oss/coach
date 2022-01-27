20d96df1520ced20299de6678c7ba77b61216a2a
Gabriela Ossowska <gab.ossowska@gmail.com>
2022-01-27 22:41:50 +0100

    new params for rainbow

--- rl_coach/presets/Doom_Basic_QR_DQN_New_Params.py
+++ rl_coach/presets/Doom_Basic_QR_DQN_New_Params.py
@@ -1,17 +1,13 @@
+from rl_coach.agents.qr_dqn_agent import QuantileRegressionDQNAgentParameters
 from rl_coach.agents.rainbow_dqn_agent import RainbowDQNAgentParameters
 from rl_coach.base_parameters import VisualizationParameters, PresetValidationParameters
-from rl_coach.core_types import CsvDataset, TrainingSteps, EnvironmentEpisodes, EnvironmentSteps
+from rl_coach.core_types import TrainingSteps, EnvironmentEpisodes, EnvironmentSteps
 from rl_coach.environments.doom_environment import DoomEnvironmentParameters
-from rl_coach.exploration_policies.bootstrapped import BootstrappedParameters
-from rl_coach.exploration_policies.greedy import GreedyParameters
-from rl_coach.exploration_policies.parameter_noise import ParameterNoiseParameters
+from rl_coach.environments.gym_environment import GymVectorEnvironment
 from rl_coach.graph_managers.basic_rl_graph_manager import BasicRLGraphManager
 from rl_coach.graph_managers.graph_manager import ScheduleParameters
 from rl_coach.memories.memory import MemoryGranularity
 from rl_coach.schedules import LinearSchedule
-
-# change parameters after analizing previous experimets
-# then use in experiments with different levels
 
 ####################
 # Graph Scheduling #
@@ -19,35 +15,33 @@ from rl_coach.schedules import LinearSchedule
 
 schedule_params = ScheduleParameters()
 schedule_params.improve_steps = TrainingSteps(2500)
-schedule_params.steps_between_evaluation_periods = EnvironmentEpisodes(10) 
+schedule_params.steps_between_evaluation_periods = EnvironmentEpisodes(10)
 schedule_params.evaluation_steps = EnvironmentEpisodes(1)
 schedule_params.heatup_steps = EnvironmentSteps(1000)
-
 
 #########
 # Agent #
 #########
-agent_params = RainbowDQNAgentParameters()
+agent_params = QuantileRegressionDQNAgentParameters()
 
 # DQN params
 agent_params.algorithm.num_steps_between_copying_online_weights_to_target = EnvironmentSteps(100)
-agent_params.algorithm.discount = 0.33
+agent_params.algorithm.discount = 0.99
 agent_params.algorithm.num_consecutive_playing_steps = EnvironmentSteps(1)
-agent_params.algorithm.n_step = 7
+agent_params.algorithm.atoms = 50
 
 # NN configuration
-agent_params.network_wrappers['main'].learning_rate = 0.66661
-agent_params.network_wrappers['main'].replace_mse_with_huber_loss = False
+agent_params.network_wrappers['main'].learning_rate = 0.00025
 
 # ER size
 agent_params.memory.max_size = (MemoryGranularity.Transitions, 40000)
 
-agent_params.memory.beta = LinearSchedule(0.4, 1, 10000)
-agent_params.memory.alpha = 0.5
+# E-Greedy schedule
+agent_params.exploration.epsilon_schedule = LinearSchedule(1.0, 0.01, 10000)
 
-###############
-# Environment #
-###############
+################
+#  Environment #
+################
 env_params = DoomEnvironmentParameters(level='basic')
 
 ########
