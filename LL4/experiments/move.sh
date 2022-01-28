559a174ce36e5aa976855b49a58f3deeee9ef010
Gabriela Ossowska <gab.ossowska@gmail.com>
2022-01-28 23:04:07 +0100

    replace results of experiments if they already exist

--- LL4/experiments/move.sh
+++ LL4/experiments/move.sh
@@ -3,10 +3,10 @@
 # move results (csv) to one folder for every type of experiment, ./[experiment_type]/results/
 for exp_type in */ ; do
 	cd "${exp_type}/experiments"
-	mkdir ../results
+	mkdir -p ../results
 	for experiment in */ ; do
 		cd $experiment
-		cp worker_0.simple_rl_graph.main_level.main_level.agent_0.csv "../../results/${experiment::-1}.csv"
+		cp -rf worker_0.simple_rl_graph.main_level.main_level.agent_0.csv "../../results/${experiment::-1}.csv"
 		cd ..
 	done
 	cd ../..
--- LL4/experiments/move_levels_results.sh
+++ LL4/experiments/move_levels_results.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+# move results of running presets on different levels into one folder, ./level_results
 for exp_type in Doom_*/ ; do
 	cd "${exp_type}/experiments"
 	mkdir -p ../../levels_results
 	for experiment in */ ; do
 		cd $experiment
-		cp worker_0.simple_rl_graph.main_level.main_level.agent_0.csv "../../../levels_results/${exp_type::-1}.csv"
+		cp -rf worker_0.simple_rl_graph.main_level.main_level.agent_0.csv "../../../levels_results/${exp_type::-1}.csv"
 		cd ..
 	done
 	cd ../..
